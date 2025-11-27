66c24134eb2b41f363afcb7197333161ea67b8e6
Prince Sinha <princesinha629@gmail.com>
2025-11-27 16:47:58 +0530

    Footer Implemented

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 const Header = () => {
   return (
     <header className='p-4 border-b '>
-      <div className='flex  flex-wrap sm:flex-nowrap items-center justify-between px-2'>
-        <nav className='flex gap-4 overflow-x-auto no-scrollbar'>
+      <div className='flex flex-wrap sm:flex-nowrap items-center justify-around px-2 '>
+        <nav className='flex gap-12 overflow-x-auto no-scrollbar justify-between '>
          {HeaderItems.map((item,idx) => (
            <Link href={item.href} className='whitespace-nowrap hover:text-red-700 font-semibold border-b-2 border-transparent pb-1 hover:border-b-red-700 cursor-pointer text-sm sm:text-base' key={idx}>{item.name}
             </Link>
